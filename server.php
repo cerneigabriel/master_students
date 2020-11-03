28b54d4c9802eff98d9c49dba83647a6501c0ba0
cerneigabriel <40870657+cerneigabriel@users.noreply.github.com>
2020-11-03 11:48:18 +0200

    modify htaccess

--- server.php
+++ server.php
@@ -9,14 +9,13 @@ define("LAYOUTS_PATH", VIEWS_PATH . "layouts/");
 define("CONFIG_PATH", BASE_PATH . "config/");
 
 
-echo $_SERVER;
 require_once VENDOR_PATH . "autoload.php";
 require_once APP_PATH . "helpers.php";
 
-// use MasterStudents\Core\Application;
+use MasterStudents\Core\Application;
 
-// $app = new Application();
+$app = new Application();
 
-// require_once ROUTES_PATH . "web.php";
+require_once ROUTES_PATH . "web.php";
 
-// $app->run();
+$app->run();

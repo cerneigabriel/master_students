cf6265384fd03a623aa37b41151065b0732a5d8f
cerneigabriel <40870657+cerneigabriel@users.noreply.github.com>
2020-11-10 21:11:30 +0200

    Prepare credentials for database connection

--- config/db.php
+++ config/db.php
@@ -1,13 +1,9 @@
 <?php
 
 return [
-    // "connection" => "",
-    "host" => "ec2-54-247-94-127.eu-west-1.compute.amazonaws.com",
-    "port" => "5432",
-    "database" => "db45c8qg5r3vg3",
-    "user" => "dalcqxbbtenejb",
-    "password" => "0e3e6842f0594bcbea8bc3fc0eaa8abeba15a832720d1c635c36da4a47321201",
-    "uri" => "postgres://dalcqxbbtenejb:0e3e6842f0594bcbea8bc3fc0eaa8abeba15a832720d1c635c36da4a47321201@ec2-54-247-94-127.eu-west-1.compute.amazonaws.com:5432/db45c8qg5r3vg3",
-    "heroku_cli" => "heroku pg:psql postgresql-convex-40737 --app masterstudents
-    "
+    'dbname' => env("DB_DATABASE"),
+    'user' => env("DB_USERNAME"),
+    'password' => env("DB_PASSWORD"),
+    'host' => env("DB_HOST"),
+    'driver' => env("DB_CONNECTION"),
 ];

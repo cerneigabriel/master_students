902584b33f3903882d2985a59f781e8c1ea826ae
cerneigabriel <40870657+cerneigabriel@users.noreply.github.com>
2020-11-10 21:34:41 +0200

    Show all errors

--- server.php
+++ server.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define("BASE_PATH", __DIR__ . "/");
 define("APP_PATH", BASE_PATH . "app/");
 define("VENDOR_PATH", BASE_PATH . "vendor/");
